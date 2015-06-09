dca6176b2d4b03804c88eda1ff98221ec3f6ad85
Ollirmm <o.heckhuis@gmx.de>
2015-06-09 20:39:05 +0200

    class Game barebones

--- game.cpp
+++ game.cpp
@@ -1,1 +1,11 @@
 #include game.h
+
+void Game::Game(GameScene scene){
+	myScene = scene;
+	myScene.initScene();
+	
+}
+
+void Game::gameStep(){
+}
+void Game::turn(int player, bool &finish, bool &win)
--- game.h
+++ game.h
@@ -1,7 +1,22 @@
 #ifndef THEGAME
 #define THEGAME
+#include "GameScene.h"
 class Game {
-	int gamestate;
+public:
+	
+	int gameState;
+	
+	bool turn; // false = p1 | true = p2
+	bool watch; // false = watch | true = play;
+	bool finish = false;
+	
+	GameScene myScene;
+	
+	Game();
+	void gameStep();
+	
+	
+	private void turn(int player, bool &finish,bool &win);
 	
 }
 #endif THEGAME
